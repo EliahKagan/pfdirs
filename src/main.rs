29774c069cffa6ed9a46b8dfe963936aaf9af1ab
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-08 04:48:56 -0400

    Show relevant environment variables
    This doesn't make use (except through the Rust standard library) of
    the Windows API yet, but this commit also adds the windows crate,
    to be used soon.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+//! Reports information from multiple sources about where `Program Files`
+//! folders are located on a Windows system.
+
+fn column_width(names: &[&str]) -> usize {
+    names
+        .iter()
+        .map(|name| name.chars().count())
+        .max()
+        .unwrap_or(0)
+}
+
+fn report_environment_variables() {
+    let names = [
+        "ProgramFiles",
+        "ProgramFiles(Arm)",
+        "ProgramFiles(x86)",
+        "ProgramW6432",
+    ];
+
+    let width = column_width(&names);
+
+    println!("Relevant environment variables:");
+    println!();
+
+    for name in names {
+        let path_item = std::env::var(name).unwrap_or_else(|_| "[variable does not exist]".into());
+        println!("  {name:<width$}  {path_item}");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    report_environment_variables();
+
+    // FIXME: Do the other reports.
 }
