32cdbf4359bcbbfee8651525da24f8bf91890a3c
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-11 04:49:36 -0400

    Start on detailed documentation comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,87 @@
-//! Reports information from multiple sources about where Program Files folders
-//! are located on a Windows system.
+//! Reports information from multiple sources about where program files directories are located on
+//! a Windows system.
+//!
+//! Example output, from a 32-bit process running on an x86_64 Windows system:
+//!
+//! ```text
+//! Relevant environment variables:
+//!
+//!   ProgramFiles       C:\Program Files (x86)
+//!   ProgramFiles(Arm)  [environment variable not found]
+//!   ProgramFiles(x86)  C:\Program Files (x86)
+//!   ProgramW6432       C:\Program Files
+//!
+//! Relevant known folders:
+//!
+//!   FOLDERID_ProgramFiles      C:\Program Files (x86)
+//!   FOLDERID_ProgramFilesX64   [The system cannot find the file specified. (0x80070002)]
+//!   FOLDERID_ProgramFilesX86   C:\Program Files (x86)
+//!   FOLDERID_UserProgramFiles  C:\Users\ek\AppData\Local\Programs
+//!
+//! Relevant CSIDLs:
+//!
+//!   CSIDL_PROGRAM_FILES     C:\Program Files (x86)
+//!   CSIDL_PROGRAM_FILESX86  C:\Program Files (x86)
+//!
+//! Relevant registry keys - with default view:
+//!
+//!   ProgramFilesDir        C:\Program Files (x86)
+//!   ProgramFilesDir (Arm)  [The system cannot find the file specified. (os error 2)]
+//!   ProgramFilesDir (x86)  C:\Program Files (x86)
+//!   ProgramW6432Dir        C:\Program Files
+//!
+//! Relevant registry keys - with KEY_WOW64_32KEY:
+//!
+//!   ProgramFilesDir        C:\Program Files (x86)
+//!   ProgramFilesDir (Arm)  [The system cannot find the file specified. (os error 2)]
+//!   ProgramFilesDir (x86)  C:\Program Files (x86)
+//!   ProgramW6432Dir        C:\Program Files
+//!
+//! Relevant registry keys - with KEY_WOW64_64KEY:
+//!
+//!   ProgramFilesDir        C:\Program Files
+//!   ProgramFilesDir (Arm)  [The system cannot find the file specified. (os error 2)]
+//!   ProgramFilesDir (x86)  C:\Program Files (x86)
+//!   ProgramW6432Dir        C:\Program Files
+//! ```
+//!
+//! On 64-bit Windows, the `ProgramFiles` environment variable, `FOLDERID_ProgramFiles` known
+//! folder, `CSIDL_PROGRAM_FILES`, and `ProgramFilesDir` registry key, look up a path that differs
+//! depending on whether the program accessing the information is 64-bit or 32-bit.
+//!
+//! On such a system, whether x86_64 (AMD64) or ARM64, a 64-bit process reports the 64-bit program
+//! files directory, most often `C:\Program Files`, while a 32-bit process reports the 32-bit
+//! program files directory, most often `C:\Program Files (x86)`.
+//!
+//! In contrast, *when available*:
+//!
+//! - The `ProgramFiles(x86)` environment variable, `FOLDERID_ProgramFilesX86` known folder,
+//!   `CSIDL_PROGRAM_FILESX86`, and `ProgramFilesDir (x86)` registry key report the 32-bit program
+//!   files directory.
+//!
+//! - The `ProgramW6432` environment variable, `FOLDERID_ProgramFilesX64` known folder, and
+//!   `ProgramW6432Dir` registry key report the 64-bit program files directory.
+//!
+//! However, not all of them are always available to all processes on all Windows system.
+//!
+//! As detailed in comments below on specific `report_*` functions below, Microsoft documentation
+//! tends to recommend obtaining such paths through the *known folders* facilities. However, as
+//! shown above, even on a 64-bit system, a 32-bit process unfortunately does not see any
+//! `FOLDERID_ProgramFilesX64` known folder (and there is no CSIDL corresponding to that).
+//!
+//! On such a system it may therefore be necessary to use either the `ProgramW6432` environment
+//! variable or the `ProgramW6432Dir` registry key to get the path of the 64-bit program files
+//! directory:
+//!
+//! - Accessing the environment variable is easy and seems to be more common. Some forms of unusual
+//!   customization by a parent process of its child processes' environments will break this. See
+//!   `report_environment_variables()` below for details.
+//!
+//! - The `ProgramW6432Dir` key appears to be available on 64-bit systems through any registry
+//!   view.
+//!
+//! On a 32-bit system, there is no way to get the 64-bit program files directory, because there is
+//! no such directory.
 
 use core::ffi::c_void;
 use std::{io, string::FromUtf16Error};
@@ -22,6 +104,11 @@ use winreg::{
     RegKey,
 };
 
+
+/// Finds the width of the symbolic name column for the table of reported results.
+///
+/// This estimate is highly likely to be accurate, since the names in this column are known in
+/// advance. (They even happen to be all ASCII characters, though this does not rely on that.)
 fn column_width<'a, I>(names: I) -> usize
 where
     I: IntoIterator<Item = &'a str>,
@@ -33,6 +120,9 @@ where
         .unwrap_or(0)
 }
 
+/// Report *program files* folder locations contained in environment variables.
+///
+/// FIXME: Write the rest of this documentation comment!!
 fn report_environment_variables() {
     let names = [
         "ProgramFiles",
@@ -53,6 +143,7 @@ fn report_environment_variables() {
     println!();
 }
 
+/// Owner of a `PWSTR` that must be freed with `CoTaskMemFree`.
 struct CoStr {
     pwstr: PWSTR,
 }
@@ -74,7 +165,9 @@ impl Drop for CoStr {
     }
 }
 
-// TODO: Figure out if we should also check with other flags than KF_FLAG_DEFAULT.
+/// Helper that calls `ShGetKnownFolderPath` on behalf of `report_known_folders()`.
+///
+/// TODO: Figure out if we should also check with other flags than KF_FLAG_DEFAULT.
 fn get_known_folder_path_or_detailed_error(id: GUID) -> Result<String, Error> {
     match unsafe { SHGetKnownFolderPath(&id, KF_FLAG_DEFAULT, None) } {
         Ok(pwstr) => Ok(CoStr::new(pwstr).to_string()?),
@@ -82,10 +175,51 @@ fn get_known_folder_path_or_detailed_error(id: GUID) -> Result<String, Error> {
     }
 }
 
-/// Obtain information about known folders using both the `known-folders` crate and by manually
-/// using the Windows API via the `windows` crate, and compare the results. Doing both is for
-/// experimentation and demonstration purposes and not a reasonable production technique.
-/// Generally at most one of these two approaches should be used, depending on requirements.
+/// Report *program files* folder locations by querying *known folders*.
+///
+/// This is a recommended approach. This can be done through the Windows API or indirectly through
+/// a crate that wraps it. This function showcases both and asserts that the information provided,
+/// where overlapping, is identical.
+///
+/// #### Windows API
+///
+/// Windows provides two approaches in its API for accessing the paths of known folders:
+///
+/// - The
+///   [`SHGetKnownFolderPath`](https://learn.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-shgetknownfolderpath)
+///   function. This approach is more straightforward and typically sufficient when the GUIDs are
+///   known and only paths are needed. (There are a small number of other related functions for
+///   obtaining other information.) This is the approach used here.
+///
+/// - The
+///   [`IKnownFolder::GetPath`](https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-iknownfolder-getpath)
+///   method. This is more involved, but `IKnownFolder` COM objects are a richer source of
+///   information. For example, `IKnownFolder` supports iterating over all known folders.
+///
+/// #### known-folders crate
+///
+/// The [known-folders](https://crates.io/crates/known-folders) crate provides a
+/// `get_known_folder_path()` function that takes care of calling `SHGetKnownFolderPath` from Rust
+/// code. However, this is limited to simple uses:
+///
+/// - It does not accept custom `KNOWN_FOLDER_FLAGS` or a custom access token.
+///
+/// - It returns an `Option` rather than a `Result`, so when a known folder path is unavailable,
+///   the different errors that can cause this are not distinguished.
+///
+/// But in the most common cases `get_known_folder_path()` is sufficient.
+///
+/// #### What this function does
+///
+/// This uses both `SHGetKnownFolderPath`, called through the `windows` crate, and
+/// `get_known_folder_path()`, provided by the `known-folders` crate, and compares the results for
+/// whether there was an error and, if not, whether the paths match. Calling both is for
+/// experimentation and demonstration purposes. Generally at most one of these two approaches
+/// should be used, depending on requirements.
+///
+/// This looks up only the four folder IDs for *program files* folders. Their GUIDs are available
+/// as symbolic constants both in the `windows` crate as `GUID` objects and, as a higher level
+/// abstraction, in the `KnownFolder` enum of the `known-folders` crate.
 fn report_known_folders() -> Result<(), Error> {
     // TODO: If we can get the names without initializing COM, do so and display them as well.
     let folders = [
@@ -139,6 +273,7 @@ fn report_known_folders() -> Result<(), Error> {
     Ok(())
 }
 
+/// Helper that calls `SHGetFolderPathW()` on behalf of `report_csidl()`.
 fn try_get_path_from_csidl(csidl: u32) -> Result<String, Error> {
     let mut buffer = [0u16; MAX_PATH as usize];
 
@@ -157,6 +292,38 @@ fn try_get_path_from_csidl(csidl: u32) -> Result<String, Error> {
     Ok(path)
 }
 
+/// Report *program files* folder locations via lookups using CSIDLs.
+///
+/// This calls the deprecated
+/// [`SHGetFolderPathW`](https://learn.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-shgetfolderpathw)
+/// function.
+///
+/// This is the older way, before the *known folders* facilities were introduced. See
+/// [CSIDL](https://learn.microsoft.com/en-us/windows/win32/shell/csidl).
+///
+/// As noted there, it is recommended to use the known folders APIs instead of CSIDLs, and each
+/// CSIDL value has a corresponding `KNOWNFOLDERID` value. In contrast, not all known folders have
+/// a CSIDL, and also, unlike with CSIDLs, it is possible to register new known folders
+/// programmatically.
+///
+/// From the [remarks section](https://learn.microsoft.com/en-us/windows/win32/shell/csidl#remarks)
+/// of that article:
+///
+/// > These values supersede the use of environment variables for this purpose. They are in turn
+/// > superseded in Windows Vista and later by the
+/// > [KNOWNFOLDERID](https://learn.microsoft.com/en-us/windows/win32/shell/knownfolderid) values.
+///
+/// (This seems to imply, by transitivity, that getting the paths of known folders is also
+/// preferable to accessing the values of environment variables, when both are applicable.)
+///
+/// One limitation of using CSIDLs is that it cannot properly handle the unusual case that the path
+/// is a `\\?\` long path and exceeds
+/// [`MAX_PATH`](https://learn.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation)
+/// characters. As
+/// [commented](https://github.com/dotnet/runtime/blob/v8.0.7/src/libraries/System.Private.CoreLib/src/System/Environment.Win32.cs#L210-L211)
+/// in the implementation of the .NET Runtime:
+///
+/// > We're using SHGetKnownFolderPath instead of SHGetFolderPath as SHGetFolderPath is capped at MAX_PATH.
 fn report_csidl() -> Result<(), Error> {
     let folders = [
         ("CSIDL_PROGRAM_FILES", CSIDL_PROGRAM_FILES), // Corresponds to: FOLDERID_ProgramFiles
@@ -176,6 +343,13 @@ fn report_csidl() -> Result<(), Error> {
     Ok(())
 }
 
+/// Report *program files* folder locations from a single specified view of the registry.
+///
+/// See `report_all_registry_views()` for more information on views.
+///
+/// This accesses subkeys of `HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion` using the `winreg`
+/// crate, which uses
+/// [`RegOpenKeyExW`](https://learn.microsoft.com/en-us/windows/win32/api/winreg/nf-winreg-regopenkeyexw).
 fn report_registry_view(caption: &str, flag_for_view: u32) -> Result<(), io::Error> {
     let key_names = [
         "ProgramFilesDir",
@@ -205,6 +379,14 @@ fn report_registry_view(caption: &str, flag_for_view: u32) -> Result<(), io::Err
     Ok(())
 }
 
+/// Report *program files* folder locations from multiple views of the registry.
+///
+/// See also:
+///
+/// - [Accessing an Alternate Registry View](https://learn.microsoft.com/en-us/windows/win32/winprog64/accessing-an-alternate-registry-view)
+///   for details on registry views that can be accessed.
+///
+/// - `report_registry_view()` for details on how the lookup is performed.
 fn report_all_registry_views() -> Result<(), io::Error> {
     let views = [
         ("default view", 0),
